b8ffc4509668b0ef07afafac94ace938e84353d4
Alex Aravo <yoshino000501@outlook.com>
2020-04-30 01:03:29 +0800

    Optimized For Mainland Chinese users
    - Use ipv4.icanhazip.com to get local IPv4 IPAddr
    - Use myip.ipip.net to get detailed IPaddr Info
    - Add Update Time
    - Minor Code Adjustment

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# cloudflare-ddns-updater
+# cloudflare-ddns-updater for ipv4 only
 Quick bash script to update cloudflare's dynamic dns
+Optimized For Mainland Chinese users
+  - Use ipv4.icanhazip.com to get local IPv4 IPAddr
+  - Use myip.ipip.net to get detailed IPaddr Info
+  - Add Update Time
 
 will update IP if change is detected (run it periodically in crontab). --force and --log available. 
 --log logs to ~/log-cfddns
+--force force to update Cloudflare's Record
--- cfddns.sh
+++ cfddns.sh
@@ -1,71 +1,79 @@
 #!/bin/bash
 
 #fill these in
-email="mail@email.com"
-key="4234234FAKE593534FAKE6d"
-zone="example.com"
-record="sub.example.com"
+email="Your Cloudflare Email"
+key="Global Key"
+zone="your.domain"
+record="example.your.domain"
 #
 
-
 for opt in "$@"; do
-	case $opt in 
-	--log)
-		log=1
-		;;
-	--force)
-		force=1
-		;;
-	esac
-done 
+  case $opt in
+  --log)
+    log=1
+    ;;
+  --force)
+    force=1
+    ;;
+  esac
+done
 
-ipaddr=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
+# ipaddr=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
+ipaddr=$(curl -s ipv4.icanhazip.com)
 cfarec=$(dig $record +short)
+ipipaddr=$(curl -s myip.ipip.net)
+time=$(date)
 
 if [ "$force" != 1 ]; then
-	if [ $ipaddr == $cfarec ]; then 
-		echo "IP $ipaddr unchanged"
-		updateip=0
-	else
-		updateip=1
-	fi
+  if [ $ipaddr == $cfarec ]; then
+    echo -e Time:[$(date)] IPAddr:[$ipipaddr] Status:[Unchanged] '\n'
+    updateip=0
+  else
+    updateip=1
+  fi
 else
-	echo "forcing IP update to: $ipaddr"
-	updateip=1
+  echo "forcing IP update to: $ipaddr"
+  updateip=1
 fi
 
 if [ "$updateip" == 1 ]; then
-	zidreq=$(curl -sS "https://api.cloudflare.com/client/v4/zones?name=$zone" \
-		-H "X-Auth-Email: $email" \
-		-H "X-Auth-Key: $key" \
-		-H "Content-Type: application/json")
-	zid=$(echo $zidreq | grep -Po '(?<="id":")[^"]*' | head -1)
+  zidreq=$(
+    curl -sS "https://api.cloudflare.com/client/v4/zones?name=$zone" \
+    -H "X-Auth-Email: $email" \
+    -H "X-Auth-Key: $key" \
+    -H "Content-Type: application/json"
+  )
+  zid=$(echo $zidreq | grep -Po '(?<="id":")[^"]*' | head -1)
 
-	ridreq=$(curl -sS "https://api.cloudflare.com/client/v4/zones/$zid/dns_records?name=$record" \
-		-H "X-Auth-Email: $email" \
-		-H "X-Auth-Key: $key" \
-		-H "Content-Type: application/json") 
-	rid=$(echo $ridreq | grep -Po '(?<="id":")[^"]*')
+  ridreq=$(
+    curl -sS "https://api.cloudflare.com/client/v4/zones/$zid/dns_records?name=$record" \
+    -H "X-Auth-Email: $email" \
+    -H "X-Auth-Key: $key" \
+    -H "Content-Type: application/json"
+  )
+  rid=$(echo $ridreq | grep -Po '(?<="id":")[^"]*')
 
-	update=$(curl -sSX PUT "https://api.cloudflare.com/client/v4/zones/$zid/dns_records/$rid" \
-     	-H "X-Auth-Email: $email" \
-     	-H "X-Auth-Key: $key" \
-     	-H "Content-Type: application/json" \
-     	--data '{"type":"A","name":"'${record}'","content":"'${ipaddr}'","ttl":1,"proxied":false}')
+  update=$(
+    curl -sSX PUT "https://api.cloudflare.com/client/v4/zones/$zid/dns_records/$rid" \
+    -H "X-Auth-Email: $email" \
+    -H "X-Auth-Key: $key" \
+    -H "Content-Type: application/json" \
+    --data '{"type":"A","name":"'${record}'","content":"'${ipaddr}'","ttl":1,"proxied":false}'
+  )
 fi
 
 if [ "$log" == 1 ]; then
-	printf "Current IP for $record is: $cfarec\n" | tee -a ~/log-cfddns
-	printf "Current public IP is: $ipaddr\n\n" | tee -a ~/log-cfddns
-	if [ "$updateip" == 1 ]; then 
-		printf "$zidreq\n\n" >> ~/log-cfddns
-		printf "$ridreq\n\n" >> ~/log-cfddns
-		printf "$update" | tee -a ~/log-cfddns
-	else
-		printf "IP has not changed, no requests made to cloudflare\n\n" >> ~/log-cfddns
-	fi
-	printf "\n\nSee file: ~/log-cfddns for details\n\n" 
-	exit 0
+  printf "Current Time： $time\n" | tee -a ~/log-cfddns
+  printf "Current IP for $record is: $cfarec\n" | tee -a ~/log-cfddns
+  printf "Current public IP is: $ipaddr\n\n" | tee -a ~/log-cfddns
+  if [ "$updateip" == 1 ]; then
+    printf "$zidreq\n\n" >>~/log-cfddns
+    printf "$ridreq\n\n" >>~/log-cfddns
+    printf "$update" | tee -a ~/log-cfddns
+  else
+    printf "IP has not changed, no requests made to cloudflare\n\n\n" >>~/log-cfddns
+  fi
+  exit 0
 fi
 
 printf "$update"
